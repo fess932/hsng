ec47b3d7e53880274cc474d54f36dd5d7416ac5c
Ivan Cheremisin <fess932@gmail.com>
2021-12-26 18:59:18 +0300

    initial

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/fess932/gqlgen-todos/graph"
+	"github.com/fess932/gqlgen-todos/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	r := chi.NewRouter()
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
